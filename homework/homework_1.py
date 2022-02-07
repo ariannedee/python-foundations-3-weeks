2a3e4816a77e280cc9c1754f8153f86f698716dc
ariannedee <ariannedee@gmail.com>
2022-02-06 18:12:47 -0800

    Add homework todos

---
+++ homework/homework_1.py
@@ -0,0 +1,17 @@
+# Todo: Create a c_to_f(temp) function
+
+
+# Todo: Create a list of reminders
+
+
+# Todo: Get some input from the user
+
+
+# Todo: Create a string with email content that includes:
+#       - A greeting
+#       - The day's weather + temp
+#       - Some reminders
+#       - 2-3 more items based on APIs that you've found
+
+
+# Todo: Print the content to the console
