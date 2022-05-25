922135f65f4241f30b16b3f14cbe257fa12b6f5e
ariannedee <ariannedee@gmail.com>
2022-05-24 23:10:04 -0700

    Update practice files

---
+++ practice/week_2/practice_1_cli.py
@@ -0,0 +1,16 @@
+"""
+Create a command-line application that:
+- Prompts the user for inputs
+- Handles invalid inputs
+- Do some calculation or get data from an API
+- Print result, save to file, or open web page
+
+Some ideas:
+- Expand any of the practice projects from week 1
+- Text translator using googletrans
+- SMS text sender using twilio (costs $)
+- Automated birthday greeter (text or email)
+- Random password generator
+- Currency converter
+- Random Wikipedia article
+"""
