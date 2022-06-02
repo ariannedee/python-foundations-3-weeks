6c26df0761e97170cd023d650709ba5eaa3460e2
ariannedee <ariannedee@gmail.com>
2022-06-02 14:13:35 -0700

    Add practice

---
+++ practice/week_2/practice_2_wordle.py
@@ -0,0 +1,19 @@
+"""
+Create a complete Wordle clone.
+https://www.nytimes.com/games/wordle/index.html
+
+It should:
+- Get a random word from a file
+- Ask for user input
+- Validate that it's a valid word of the right length
+- Print the result that shows letters in the right place, wrong place and not in word
+- Give the user 6 tries to get the right answer
+
+You can also print out the alphabet with each letter as:
+- grey if not guessed
+- black if guessed and not in word
+- green if guessed and in the right spot
+- yellow if guessed and in the wrong spot
+
+You could keep track of the user's guess distribution and streak by storing data in a file or database.
+"""
