12878599d5543322ed91fc883cd13f0574e8eaec
ariannedee <ariannedee@gmail.com>
2022-11-30 15:49:28 -0800

    Remove quizzes

--- quizzes/quiz1.py
+++
@@ -1,4 +0,0 @@
-from quiz import get_questions, Quiz
-
-questions = get_questions(1)
-Quiz(questions).run_quiz()
--- quizzes/quiz2.py
+++
@@ -1,4 +0,0 @@
-from quiz import get_questions, Quiz
-
-questions = get_questions(2)
-Quiz(questions).run_quiz()
