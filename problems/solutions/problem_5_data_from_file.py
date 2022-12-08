869524f4d187a88b28c694e755b309bb16e2a90b
ariannedee <ariannedee@gmail.com>
2022-12-08 12:13:37 -0800

    Solution from class Dec 8

--- problems/solutions/problem_5_data_from_file.py
+++ problems/solutions/problem_5_data_from_file.py
@@ -1,14 +1,29 @@
-from csv import DictReader
+import csv
+from collections import defaultdict
 
-from problem_4_display_apis import display_by_category
+def print_by_category(api_list, num_per_page=None):
+    apis_by_category = defaultdict(list)
+
+    for api in api_list:
+        category = api['Category']
+        apis_by_category[category].append(api)
+
+    for category, apis in apis_by_category.items():
+        print(category.upper())
+        print()
+        for i, api in enumerate(apis, start=1):
+            print(f"{api['API']}: {api['Link']}")
+            if num_per_page and i % num_per_page == 0:
+                input()
+        input()
 
 while True:
     try:
-        num_results = int(input("How many results to show at a time? "))
+        num_per_page = int(input("How many results to display at a time? "))
         break
     except ValueError:
         print("Invalid number")
 
 with open('data/apis.csv', 'r') as file:
-    reader = DictReader(file)
-    display_by_category(reader, num_results)
+    reader = csv.DictReader(file)
+    print_by_category(reader, num_per_page)
