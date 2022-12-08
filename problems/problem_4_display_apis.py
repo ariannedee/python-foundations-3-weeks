0f8f2ba2535bc2c27f604b08f6586a3df5822902
ariannedee <ariannedee@gmail.com>
2022-12-08 09:19:07 -0800

    Add hardcoded list of apis

---
+++ problems/problem_4_display_apis.py
@@ -0,0 +1,10 @@
+sample_apis = [
+    {'Name': 'Cat facts', 'URL': 'https://alexwohlbruck.github.io/cat-facts/', 'Category': 'Animal'},
+    {'Name': 'Dog facts', 'URL': 'https://dukengn.github.io/Dog-facts-API/', 'Category': 'Animal'},
+    {'Name': 'Random dog', 'URL': 'https://random.dog/woof.json', 'Category': 'Animal'},
+    {'Name': 'Colormind', 'URL': 'http://colormind.io/api-access/', 'Category': 'Art & Design'},
+    {'Name': 'Pixel Encounter', 'URL': 'https://pixelencounter.com/api', 'Category': 'Art & Design'},
+    {'Name': 'PoetryDB', 'URL': 'https://github.com/thundercomb/poetrydb', 'Category': 'Books'},
+    {'Name': 'Bored', 'URL': 'https://www.boredapi.com/', 'Category': 'Development'},
+    {'Name': 'Open-Meteo', 'URL': 'https://open-meteo.com/', 'Category': 'Weather'},
+]
