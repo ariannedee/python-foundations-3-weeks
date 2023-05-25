1a2dda6d05a3bb9e4d6e2704a76529b68033b3bb
ariannedee <ariannedee@gmail.com>
2023-05-25 12:29:40 -0700

    Add solutions from class

--- problems/solutions/problem_5_data_from_file.py
+++ problems/solutions/problem_5_data_from_file.py
@@ -1,29 +1,14 @@
+"""Print out the API list, sorted by category, from data/apis.csv"""
 import csv
-from collections import defaultdict
 
-def print_by_category(api_list, num_per_page=None):
-    apis_by_category = defaultdict(list)
-
-    for api in api_list:
-        category = api['Category']
-        apis_by_category[category].append(api)
-
-    for category, apis in apis_by_category.items():
-        print(category.upper())
-        print()
-        for i, api in enumerate(apis, start=1):
-            print(f"{api['API']}: {api['Link']}")
-            if num_per_page and i % num_per_page == 0:
-                input()
-        input()
-
-while True:
-    try:
-        num_per_page = int(input("How many results to display at a time? "))
-        break
-    except ValueError:
-        print("Invalid number")
-
-with open('data/apis.csv', 'r') as file:
+with open('data/apis.csv') as file:
     reader = csv.DictReader(file)
-    print_by_category(reader, num_per_page)
+
+    current_category = None
+    for api in reader:
+        category = api['Category']
+        if current_category != category:
+            print()
+            current_category = category
+            print(category.upper())
+        print(f"{api['API']}: {api['Link']}")
