206288dc6adb4a59bf0bdeffab60052856551eed
ariannedee <ariannedee@gmail.com>
2024-01-25 09:17:00 -0800

    Update solutions for Jan 25, 2024 class

--- homework/class_solutions/homework_1.py
+++ homework/class_solutions/homework_1.py
@@ -1,39 +1,30 @@
 import random
 
 
-def c_to_f(temp_c):
-    return (temp_c * 9 / 5) + 32
+def c_to_f(temp):
+    return (temp * 9 / 5) + 32
 
-
-assert c_to_f(0) == 32, f"Got {c_to_f(0)}"
-assert round(c_to_f(36.5)) == 98, f"Got {c_to_f(0)}"
 
 reminders = [
-    "Go for a walk",
-    "Stretch",
+    'drink water',
+    'stretch',
+    'move',
 ]
 
 name = input("What's your name? ").strip().title()
+weather = input("What's the weather? ").strip().lower()
+temp_c_hi = random.randint(5, 10)
+temp_c_lo = random.randint(-2, 4)
 
-weather = ["sunny", "cloudy", "rainy"]
-
-today_weather = random.choice(weather)
-temp_c_high = random.randint(10, 25)
-temp_c_low = random.randint(0, 10)
-
-
-content = f"""Hello {name},
-
-Today is going to be {today_weather}.
-
-High of {temp_c_high}°C ({c_to_f(temp_c_high)}°F) 
-Low of {temp_c_low}°C ({c_to_f(temp_c_low)}°F) 
+greeting = f"""Good morning, {name}!
+Today is going to be {weather}.
+High: {temp_c_hi}°C ({c_to_f(temp_c_hi)}°F)'
+Low: {temp_c_lo}°C ({c_to_f(temp_c_lo)}°F)'
 
 Remember to:
 """
 
 for reminder in reminders:
-    content += f"- {reminder}\n"
+    greeting += f"- {reminder}\n"
 
-
-print(content)
+print(greeting)
