ddd19b8b18166578586016d98dc197606d4d9a2b
ariannedee <ariannedee@gmail.com>
2024-05-16 09:25:28 -0700

    Add solutions for May 2024 class

--- homework/class_solutions/homework_2.py
+++ homework/class_solutions/homework_2.py
@@ -1,28 +1,18 @@
 import sys
+from pprint import pprint
 
 import requests
 
 from weather_codes import weather_from_code
 
-try:
-    name = sys.argv[1]
-except IndexError:
-    name = input("What's your name? ")
-
-
-def c_to_f(temp):
-    return (temp * 9 / 5) + 32
-
-
-assert c_to_f(0) == 32, f"Got {c_to_f(0)}"
-assert round(c_to_f(36.5)) == 98, f"Got {c_to_f(36.5)}"
+DEBUG = False
 
 base_url = f'https://api.open-meteo.com/v1/forecast'
 
 params = {
     'timezone': 'America/Los_Angeles',
-    'latitude': 42.2497,
-    'longitude': -123.1193,
+    'latitude': 42.997262156214305,
+    'longitude': -81.20390128320294,
     'daily': ['weathercode', 'temperature_2m_max', 'temperature_2m_min', 'sunrise', 'sunset'],
     'forecast_days': 1,
 }
@@ -35,25 +25,46 @@ response = requests.get(base_url, headers=headers, params=params)
 
 data = response.json()
 
-# print(data)
+if DEBUG:
+    print(response.url)
+    pprint(data)
 
 today = data['daily']
 
+temp_high = today['temperature_2m_max'][0]
+temp_low = today['temperature_2m_min'][0]
 weather_code = today['weathercode'][0]
-weather = weather_from_code[weather_code]
-temp_c_hi = today['temperature_2m_max'][0]
-temp_c_lo = today['temperature_2m_min'][0]
+weather = weather_from_code.get(weather_code)
 
-greeting = f"""Good morning, {name.strip().title()}!
-Today is going to be {weather.lower()}.
-High: {temp_c_hi}°C ({c_to_f(temp_c_hi)}°F)
-Low: {temp_c_lo}°C ({c_to_f(temp_c_lo)}°F)
+if weather is None:
+    print(f"Weather code {weather_code} doesn't match a known code")
+    weather = 'unknown'
+
+def c_to_f(temp_c):
+    return (temp_c * 9 / 5) + 32
+
+
+args = sys.argv
+if len(args) > 1:
+    name = ' '.join(args[1:]).title()
+else:
+    name = input('Name: ')
+
+
+temp_high_f = c_to_f(temp_high)
+temp_low_f = c_to_f(temp_low)
+
+greeting = f"""Good morning, {name.strip().title()}
+
+Today will be {weather.lower()}.
+High of {round(temp_high)} °C ({round(temp_high_f)} °F).
+Low of {round(temp_low)} °C ({round(temp_low_f)} °F)
 
 Remember to:
 """
 
 with open('reminders.txt') as file:
     for reminder in file.readlines():
-        greeting += f"- {reminder.capitalize()}"
+        greeting += f"- {reminder}"
 
 print(greeting)
