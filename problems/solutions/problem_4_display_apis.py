493a76bb4696fb44a21f0c6c67f917c1ed52eb26
ariannedee <ariannedee@gmail.com>
2025-01-14 12:03:07 -0800

    Update Jan 2025 solutions

--- problems/solutions/problem_4_display_apis.py
+++ problems/solutions/problem_4_display_apis.py
@@ -5,29 +5,33 @@ Data is hardcoded in file
 sample_apis = [
     {'Name': 'Dog facts', 'URL': 'https://dukengn.github.io/Dog-facts-API/', 'Category': 'Animal'},
     {'Name': 'PoetryDB', 'URL': 'https://github.com/thundercomb/poetrydb', 'Category': 'Books'},
-    {'Name': 'Random dog', 'URL': 'https://random.dog/woof.json', 'Category': 'Animal'},
-    {'Name': 'Open-Meteo', 'URL': 'https://open-meteo.com/', 'Category': 'Weather'},
     {'Name': 'Colormind', 'URL': 'http://colormind.io/api-access/', 'Category': 'Art & Design'},
-    {'Name': 'Cat facts', 'URL': 'https://alexwohlbruck.github.io/cat-facts/', 'Category': 'Animal'},
     {'Name': 'Pixel Encounter', 'URL': 'https://pixelencounter.com/api', 'Category': 'Art & Design'},
+    {'Name': 'Random dog', 'URL': 'https://random.dog/woof.json', 'Category': 'Animal'},
+    {'Name': 'Cat facts', 'URL': 'https://alexwohlbruck.github.io/cat-facts/', 'Category': 'Animal'},
     {'Name': 'Bored', 'URL': 'https://www.boredapi.com/', 'Category': 'Development'},
+    {'Name': 'Open-Meteo', 'URL': 'https://open-meteo.com/', 'Category': 'Weather'},
 ]
+
+
+def sort_by_category(api_dict):
+    return api_dict['Category'] + api_dict['Name']
+
+
+sample_apis.sort(key=sort_by_category)
+
+# sample_apis.sort(key=lambda api_dict: api_dict['Category'] + api_dict['Name'])
 
 # ANIMAL
 # Cat facts: https://alexwohlbruck.github.io/cat-facts/
 # Dog facts: https://dukengn.github.io/Dog-facts-API/
 
-def category_sort(api_dict):
-    return api_dict['Category'] + api_dict['Name']
-
-sample_apis.sort(key=category_sort)
-
-current_category = None
+last_category = None
 for api in sample_apis:
     category = api['Category']
-    if category != current_category:
-        if current_category is not None:
+    if last_category != category:
+        if last_category is not None:
             input()
-        current_category = category
         print(category.upper())
+    last_category = category
     print(f"{api['Name']}: {api['URL']}")
