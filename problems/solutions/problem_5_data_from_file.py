a8d0ee44fe9ce68c4cc65f2de45dfb92b816a30c
ariannedee <ariannedee@gmail.com>
2025-01-14 12:56:54 -0800

    Update Jan 2025 solutions

--- problems/solutions/problem_5_data_from_file.py
+++ problems/solutions/problem_5_data_from_file.py
@@ -4,23 +4,15 @@ Data is from data/apis.csv
 """
 import csv
 
+with open('data/apis.csv') as file:
+    apis = csv.DictReader(file)
 
-def display_apis(apis):
-    current_category = None
+    last_category = None
     for api in apis:
         category = api['Category']
-        if category != current_category:
-            print()
-            if current_category is not None:
-                input("Press Enter for next category")
-
-            current_category = category
+        if last_category != category:
+            if last_category is not None:
+                input('Enter for next category\n')
             print(category.upper())
+        last_category = category
         print(f"{api['API']}: {api['Link']}")
-
-
-with open('data/apis.csv') as file:
-    reader = csv.DictReader(file)
-    api_list = list(reader)
-
-display_apis(api_list)
