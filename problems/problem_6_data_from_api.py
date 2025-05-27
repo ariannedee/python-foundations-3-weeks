9db9f2c2c06a30a155a59346305e138f2863a4a6
ariannedee <ariannedee@gmail.com>
2025-05-27 16:02:16 -0700

    Update data source URL

--- problems/problem_6_data_from_api.py
+++ problems/problem_6_data_from_api.py
@@ -1,4 +1,4 @@
 """
 Display a list of free APIs, sorted by category
-Data is from https://api.publicapis.org/entries
+Data is from https://raw.githubusercontent.com/marcelscruz/public-apis/refs/heads/main/db/resources.json
 """
