8eb85f07490da835751ef3ed27c9509237a17de3
ariannedee <ariannedee@gmail.com>
2025-08-19 09:46:45 -0700

    Update homework for Aug 2025

--- problems/solutions/problem_3_collatz.py
+++ problems/solutions/problem_3_collatz.py
@@ -10,7 +10,7 @@ Print the Collatz sequence for each number from 1 to 100.
 
 
 def assert_equals(actual, expected):
-    """Test helper function"""
+    """Helper function for testing"""
     assert actual == expected, f"Expected {expected}, but got {actual}"
 
 
@@ -22,27 +22,27 @@ def next_num(n):
     else:
         return n * 3 + 1
 
-
 assert_equals(next_num(1), None)
+assert_equals(next_num(2), 1)
+assert_equals(next_num(3), 10)
 assert_equals(next_num(4), 2)
-assert_equals(next_num(5), 16)
 
 
 def collatz(num):
     """Return the Collatz sequence for a number (num) as a string"""
-    sequence_list = []
-    while num:
-        sequence_list.append(str(num))
-        num = next_num(num)
-
-    return " ".join(sequence_list)
-
+    col_string = str(num)
+    while True:
+        next_col = next_num(num)
+        if next_col is None:
+            break
+        col_string += f" {next_col}"
+        num = next_col
+    return col_string
 
 assert_equals(collatz(1), '1')
 assert_equals(collatz(2), '2 1')
 assert_equals(collatz(3), '3 10 5 16 8 4 2 1')
 assert_equals(collatz(4), '4 2 1')
 
-
 for i in range(100):
-    print(collatz(i))
+    print(collatz(i + 1))
