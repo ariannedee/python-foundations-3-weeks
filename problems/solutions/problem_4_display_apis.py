e43a77b500b3b9c633613d2a99c31d9525f1f25d
ariannedee <ariannedee@gmail.com>
2025-08-19 12:20:46 -0700

    Update solutions for Aug 2025

--- problems/solutions/problem_4_display_apis.py
+++ problems/solutions/problem_4_display_apis.py
@@ -3,13 +3,13 @@ Display a list of free APIs, sorted by category
 Data is hardcoded in file
 """
 sample_apis = [
-    {'Name': 'Bored', 'URL': 'https://www.boredapi.com/', 'Category': 'Development'},
     {'Name': 'Cat facts', 'URL': 'https://alexwohlbruck.github.io/cat-facts/', 'Category': 'Animal'},
     {'Name': 'Dog facts', 'URL': 'https://dukengn.github.io/Dog-facts-API/', 'Category': 'Animal'},
     {'Name': 'Random dog', 'URL': 'https://random.dog/woof.json', 'Category': 'Animal'},
     {'Name': 'Colormind', 'URL': 'http://colormind.io/api-access/', 'Category': 'Art & Design'},
     {'Name': 'Pixel Encounter', 'URL': 'https://pixelencounter.com/api', 'Category': 'Art & Design'},
     {'Name': 'PoetryDB', 'URL': 'https://github.com/thundercomb/poetrydb', 'Category': 'Books'},
+    {'Name': 'Bored', 'URL': 'https://www.boredapi.com/', 'Category': 'Development'},
     {'Name': 'Open-Meteo', 'URL': 'https://open-meteo.com/', 'Category': 'Weather'},
 ]
 
@@ -17,15 +17,15 @@ sample_apis = [
 # Cat facts: https://alexwohlbruck.github.io/cat-facts/
 # Dog facts: https://dukengn.github.io/Dog-facts-API/
 
-sample_apis.sort(key=lambda api: api["Category"])
 
-current_category = None
+def display_apis(api_list):
+    current_category = None
 
-for api in sample_apis:
-    category = api["Category"]
-    if category != current_category:
-        if current_category is not None:
-            input()
-        current_category = category
-        print(current_category.upper())
-    print(f"{api['Name']}: {api['URL']}")
+    for api in api_list:
+        category = api["Category"]
+        if category != current_category:
+            if current_category is not None:
+                input()
+            current_category = category
+            print(current_category.upper())
+        print(f"{api['Name']}: {api['URL']}")
--- problems/solutions/problem_5_data_from_file.py
+++ problems/solutions/problem_5_data_from_file.py
@@ -4,15 +4,11 @@ Data is from data/apis.csv
 """
 import csv
 
-with open('data/apis.csv') as file:
-    apis = csv.DictReader(file)
+from problem_4_display_apis import display_apis
 
-    last_category = None
-    for api in apis:
-        category = api['Category']
-        if last_category != category:
-            if last_category is not None:
-                input('Enter for next category\n')
-            print(category.upper())
-        last_category = category
-        print(f"{api['API']}: {api['Link']}")
+with open('data/apis.csv') as file:
+    file.readline()
+    fieldnames = ['Name', 'Description', 'Auth', 'HTTPS', 'Cors', 'URL', 'Category']
+    reader = csv.DictReader(file, fieldnames)
+
+    display_apis(api_list=list(reader))
