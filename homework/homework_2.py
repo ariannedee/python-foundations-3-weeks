5547975cc7071f200b1607a6306345d9d861b4d6
ariannedee <ariannedee@gmail.com>
2025-08-26 14:09:22 -0700

    Add todo items

---
+++ homework/homework_2.py
@@ -0,0 +1,7 @@
+# Todo: Copy homework_1 in
+
+# Todo: If name is passed in as a script argument, don't prompt user for name
+
+# Todo: Read reminders from a file
+
+# Todo: Get weather data from API
