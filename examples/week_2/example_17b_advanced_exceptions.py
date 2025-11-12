c23df4bc0cb0724bd3d567c941b8d96c4cfc7155
ariannedee <ariannedee@gmail.com>
2025-11-12 11:45:50 -0800

    Add raising custom error to examples

--- examples/week_2/example_17b_advanced_exceptions.py
+++ examples/week_2/example_17b_advanced_exceptions.py
@@ -26,3 +26,11 @@ else:
     print('Everything worked')  # Runs after try block finishes without errors
 finally:
     print('This always happens')  # Runs after everything, even if there was an error
+
+
+
+class MyException(Exception):
+    pass
+
+
+raise MyException("error message")
