ca70d2bbc570e6f7820b57ea6b02b0aa46447dd9
ariannedee <ariannedee@gmail.com>
2025-11-12 14:42:50 -0800

    Get Automate the Boring stuff html

--- examples/week_2/example_21_requests.py
+++ examples/week_2/example_21_requests.py
@@ -1,14 +1,13 @@
 import requests
-from requests import Response
 
-URL = "https://google.com"
+URL = "https://automatetheboringstuff.com/"
 
 # Dictionary of HTTP headers
 headers = {'User-Agent': f'Your name (your@email.com)'}
 
 # Dictionary of URL parameters
 params = {'key1': 'value1', 'key2': 'value2'}  # Gets appended to url: {url}?key1=value1&key2=value2
-response: Response = requests.get(URL, headers=headers, params=params)
+response = requests.get(URL, headers=headers, params=params)
 
 # Full list of HTTP status codes: https://en.wikipedia.org/wiki/List_of_HTTP_status_codes
 # Common status codes:
@@ -17,6 +16,7 @@ response: Response = requests.get(URL, headers=headers, params=params)
 # - 404 Not found
 # - 502 Bad gateway
 print(response.status_code)
+print(response.reason)
 
 # Raise an exception if the request was unsuccessful
 response.raise_for_status()
@@ -24,5 +24,5 @@ response.raise_for_status()
 # Get text contents
 print(response.text)
 
-with open('data/google.html', 'w', encoding="utf-8") as file:
+with open('data/atbs.html', 'w', encoding="utf-8") as file:
     file.write(response.text)
