cf413cbab9ab5085c142e21f6378ccfc7663fad7
ariannedee <ariannedee@gmail.com>
2026-04-16 09:29:40 -0700

    Update solution for Apr 2026 class

--- homework/class_solutions/homework_1.py
+++ homework/class_solutions/homework_1.py
@@ -1,32 +1,32 @@
 import random
 
 
-def c_to_f(temp_c: float) -> float:
+def c_to_f(temp_c):
     return (temp_c * 9 / 5) + 32
 
-assert c_to_f(0) == 32, f"Expected 32 but got {c_to_f(0)}"
-assert round(c_to_f(36.5)) == 98, f"Expected 98 but got {round(c_to_f(36.5))}"
+assert c_to_f(0) == 32, f"Expected {32} but got {c_to_f(0)}"
+assert round(c_to_f(36.5)) == 98, f"Expected {98} but got {round(c_to_f(36.5))}"
 
-reminders = ['Drink water', 'Stretch']
+todos = [
+    "Get groceries",
+    "Drink water",
+    "Go to gym",
+]
 
 name = input("Name: ").strip().title()
-weather = random.choice(['sunny', 'cloudy', 'rainy', 'foggy'])
-temp_high = random.randint(5, 15)
-temp_low = temp_high - random.uniform(5, 10)
+weather_conditions = ["sunny", "cloudy", "rainy"]
+weather = random.choice(weather_conditions)
+temp_c_high = random.randint(7, 20)
+temp_c_low = temp_c_high - random.randint(4, 8)
 
 content = f"""Good morning, {name}!
-
 Today is going to be {weather}.
-High: {temp_high:.0f}°C ({c_to_f(temp_high):.0f}°F)
-Low: {temp_low:.0f}°C ({c_to_f(temp_low):.0f}°F)
+High: {temp_c_high}°C ({c_to_f(temp_c_high):.0f}°F)
+Low: {temp_c_low}°C ({c_to_f(temp_c_low):.0f}°F)
 
-Daily mantra:
-{'Seize the day!'}
+Remember to:"""
 
-Remember to:
-"""
-
-for reminder in reminders:
-    content += f"- {reminder}\n"
+for todo in todos:
+    content += "\n- " + todo
 
 print(content)
